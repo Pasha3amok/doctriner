0854d09f60640b72090dad3ade7683f767a9b4b5
Pasha3amok <pasha19850@gmail.com>
2024-01-06 16:35:45 +0200

    card components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,33 @@
 import React from 'react'
-import { Box, Button, TextField } from '@mui/material'
+import { Box, Button, Card, CardContent, TextField } from '@mui/material'
 
 export default function Login() {
   return (
-	<Box sx={{
-		'& .MuiTextField-root':{
-			margin:1,
-			width:'90%'
-		}
-	}}>
-		<form noValidate>
-			<TextField 
-			label="Email"
-			name="email"
-			variant="outlined"/>
-			<TextField 
-			label="Name"
-			name="name"
-			variant="outlined"/>
-			<Button
-			type="submit"
-			variant="contained"
-			size="medium"
-			sx={{width:'90%'}}>Start</Button>
-		</form>
-	</Box>
+	<Card sx={{width:400}}>
+		<CardContent>
+			<Box sx={{
+				'& .MuiTextField-root':{
+					margin:1,
+					width:'90%'
+				}
+			}}>
+				<form noValidate>
+					<TextField 
+					label="Email"
+					name="email"
+					variant="outlined"/>
+					<TextField 
+					label="Name"
+					name="name"
+					variant="outlined"/>
+					<Button
+					type="submit"
+					variant="contained"
+					size="medium"
+					sx={{width:'90%'}}>Start</Button>
+				</form>
+			</Box>
+		</CardContent>
+	</Card>
   )
 }
