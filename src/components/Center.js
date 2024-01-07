45e96450c7d62fd93daaf1e616f88fdd9157c942
Pasha3amok <pasha19850@gmail.com>
2024-01-07 10:48:03 +0200

    add properties for center object

---
+++ src/components/Center.js
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+export default function Center(props) {
+  return (
+	<Grid container
+		  direction="column"
+		  justifyContent="center"
+		  alignItems="center"
+		  sx={{minHeight:'100vh'}}>
+		<Grid item sx={1}>
+			{props.children}
+		</Grid>
+	</Grid>
+  )
+}
